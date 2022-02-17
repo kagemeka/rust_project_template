a9037341fd60932a2624dbcd167395582c19b58f
kagemeka <kagemeka1@gmail.com>
2022-02-17 17:28:09 +0900

    update doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,31 @@
 //! This is a template crate for Rust project
+//! `//!` is a enclosing doc comment for the whole crate.
+
+pub mod sample_module {
+    //! `//!` comment can be used for the module.
+
+    /// `///` is a doc comment for the function, struct, or trait ...
+    pub fn public_fn_in_module(arg: i64) {
+        // `//` comment is not a doc comment.
+        println!(
+            "called `sample_module::public_fn_in_module()`, arg: {}",
+            arg
+        );
+    }
+}
 
 /// This is a public function.
 pub fn public_function() {
     println!("called `public_function()`");
 }
 
-// This is a private function.
-// and won't be shown in the documentation on docs.rs.
+/// This is a private function.
+/// and won't be shown in the documentation on docs.rs.
 #[allow(dead_code)]
 fn private_function() {
-    println!("called `private_function()`");
+    println!("called `private_function()`",);
     println!(
-        "called `private_function()` `private_function()` `private_function` `private_function()` `private_function()` `private_function()` `private_function()`"
+        "called `private_function()` `private_function()` `private_function`
+        `private_function()` `private_function()` `private_function()` `private_function()`"
     );
 }
-
