2b84cfae0f73c46c9a1979761114352884e84dfe
kagemeka <kagemeka1@gmail.com>
2022-02-17 16:43:00 +0900

    update format.sh

--- scripts/format.sh
+++ scripts/format.sh
@@ -8,8 +8,5 @@ cargo fmt \
     --verbose \
     --manifest-path=Cargo.toml \
     --message-format=human
-<<<<<<< HEAD
 
 ./scripts/pre-commit.sh
-=======
->>>>>>> f7698da3816fb237c30d60643092a1f2d76fff4c
